de5fe77ce2442066d25d6752ec678cd057178160
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-03-03 17:03:48 -0500

    Updates

---
+++ precache-manifest.8cda508c3fd9d63a00ddb81bc69e2239.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ba07e0b244baf51133c5380e477ea0",
+    "url": "/matthew-hernandez-project-five/index.html"
+  },
+  {
+    "revision": "e10c3a5cc749ff549ec8",
+    "url": "/matthew-hernandez-project-five/static/css/main.9b992734.chunk.css"
+  },
+  {
+    "revision": "1b37b31c8fd8480478aa",
+    "url": "/matthew-hernandez-project-five/static/js/2.d43e75b3.chunk.js"
+  },
+  {
+    "revision": "41b0c3da6a8866c17968078c7c246d41",
+    "url": "/matthew-hernandez-project-five/static/js/2.d43e75b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e10c3a5cc749ff549ec8",
+    "url": "/matthew-hernandez-project-five/static/js/main.331c4fc4.chunk.js"
+  },
+  {
+    "revision": "b104bbcaba9312de2609",
+    "url": "/matthew-hernandez-project-five/static/js/runtime-main.627183c6.js"
+  }
+]);
